c1e28b9b291cc306dc33901360c592c48ed3d493
MauricioKiniz <mksistemaseconsultoria@gmail.com>
2023-09-26 18:21:31 -0300

    Alteracoes nos testes

--- supplychain/src/test/java/com/mksistemas/supplychain/checklist/application/atualizar/AtualizarChacklistUseCaseSupplier.java
+++ supplychain/src/test/java/com/mksistemas/supplychain/checklist/application/atualizar/AtualizarChacklistUseCaseSupplier.java
@@ -1,9 +1,16 @@
 package com.mksistemas.supplychain.checklist.application.atualizar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mksistemas.supplychain.checklist.AtualizarChecklistUseCase.AtualizarChecklistRequisicao;
 import com.mksistemas.supplychain.checklist.AtualizarChecklistUseCase.ChecklistRequisicao;
 import com.mksistemas.supplychain.checklist.domain.Checklist;
 import com.mksistemas.supplychain.checklist.domain.ChecklistId;
+import com.mksistemas.supplychain.checklist.domain.ChecklistItem;
+import com.mksistemas.supplychain.checklist.domain.ChecklistItemId;
+import com.mksistemas.supplychain.checklist.domain.ValorItem;
+import com.mksistemas.supplychain.checklist.domain.ValorItemEnum;
 
 public final class AtualizarChacklistUseCaseSupplier {
 	public static AtualizarChecklistRequisicao retornarChecklistNaoExistente() {
@@ -23,4 +30,29 @@ public final class AtualizarChacklistUseCaseSupplier {
 		return Checklist.builder().withChecklistId(ChecklistId.generate())
 				.withNome("nomebase").withDescricao("descricaoBase").build();
 	}
+
+	private static List<ChecklistItem> criarItens(int total) {
+		List<ValorItem> valores = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			valores.add(ValorItem.builder()
+					.withExigeDescricao(ValorItemEnum.OBRIGATORIO)
+					.withExigeDocumento(ValorItemEnum.OBRIGATORIO)
+					.withNome("valor" + i).withSequencia(i).build());
+		}
+		List<ChecklistItem> itens = new ArrayList<>();
+		for (int i = 0; i < total; i++) {
+			itens.add(ChecklistItem.builder()
+					.withChecklistItemId(ChecklistItemId.generate())
+					.withDescricao("descricao" + i).withNome("nome" + 1)
+					.withValores(valores).build());
+		}
+		return itens;
+	}
+
+	public static Checklist retornarChecklistInsercaoItens() {
+		return Checklist.builder().withChecklistId(ChecklistId.generate())
+				.withNome("nomebase").withDescricao("descricaoBase")
+				.withItems(criarItens(5)).build();
+	}
+
 }
